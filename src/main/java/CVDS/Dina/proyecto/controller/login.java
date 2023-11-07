a83d2ab3f2fc5ee2b29c4ddf632ef770fdf306b8
GabrielSilva2111 <gasilo14@gmail.com>
2023-11-07 07:30:58 -0500

    creación de login HTML

---
+++ src/main/java/CVDS/Dina/proyecto/controller/login.java
@@ -0,0 +1,28 @@
+package CVDS.Dina.proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Login {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(String username, String password) {
+        if (username.equals("admin") && password.equals("admin")) {
+            return "redirect:/lunch";
+        } else {
+            return "login";
+        }
+    }
+    @RequestMapping("/lunch")
+    public String lunch() {
+        return "lunch";
+    }
+}
