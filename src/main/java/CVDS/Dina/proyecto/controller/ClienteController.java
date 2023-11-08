fe0bd1a5221d93b17181f6e230f9353ccc8aa5e9
Parralol <110953563+Parralol@users.noreply.github.com>
2023-11-07 23:12:31 -0500

    se puede crear usuario

--- src/main/java/CVDS/Dina/proyecto/controller/ClienteController.java
+++ src/main/java/CVDS/Dina/proyecto/controller/ClienteController.java
@@ -3,7 +3,10 @@ package CVDS.Dina.proyecto.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import CVDS.Dina.proyecto.model.Cliente;
 import CVDS.Dina.proyecto.service.ClienteService;
 
 @Controller
@@ -15,5 +18,20 @@ public class ClienteController {
     @GetMapping("/prueba")
     public String cargarDatos(){
         return "index";
+    }
+    @GetMapping("/crearcuenta")
+    public String crearCuenta(){
+        return "creacion";
+    }
+    @RequestMapping(value="/crearcuenta", method = RequestMethod.POST)
+    public String asignarCuenta(String fname, String lname, String nickname,String pass, String comidaPreferida1, String comidaPreferida2, String comidaPreferida3){
+        Cliente cliente = new Cliente(fname, lname);
+        cliente.setNickname(nickname);
+        cliente.setPassword(pass);
+        cliente.setComidaPreferidaUno(comidaPreferida1);
+        cliente.setComidaPreferidaDos(comidaPreferida2);
+        cliente.setComidaPreferidaTres(comidaPreferida3);
+        clienteService.addCliente(cliente);
+        return "creacion";
     }
 }
