397c2bddb785972b311ed93be2465aa483d6050d
Parralol <110953563+Parralol@users.noreply.github.com>
2023-11-15 17:17:49 -0500

    final cliente

--- src/main/java/CVDS/Dina/proyecto/model/Factura.java
+++ src/main/java/CVDS/Dina/proyecto/model/Factura.java
@@ -1,8 +1,136 @@
 package CVDS.Dina.proyecto.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "valor")
+    private int valor;
+
+    @Column(name = "NombreRestaurante")
+    private String nombreRes;
+
+    @Column(name = "FechaPedido")
+    private LocalDate fechaPedido;
+
+    @OneToOne(mappedBy = "factura")
+    private Pedido pedido;
+
+    
+
+    public Factura(Long id, int valor, String nombreRes, LocalDate fechaPedido) {
+        this.id = id;
+        this.valor = valor;
+        this.nombreRes = nombreRes;
+        this.fechaPedido = fechaPedido;
+    }
+
+    public Factura() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getNombreRes() {
+        return nombreRes;
+    }
+
+    public void setNombreRes(String nombreRes) {
+        this.nombreRes = nombreRes;
+    }
+
+    public LocalDate getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public void setFechaPedido(LocalDate fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + valor;
+        result = prime * result + ((nombreRes == null) ? 0 : nombreRes.hashCode());
+        result = prime * result + ((fechaPedido == null) ? 0 : fechaPedido.hashCode());
+        result = prime * result + ((pedido == null) ? 0 : pedido.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Factura other = (Factura) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (valor != other.valor)
+            return false;
+        if (nombreRes == null) {
+            if (other.nombreRes != null)
+                return false;
+        } else if (!nombreRes.equals(other.nombreRes))
+            return false;
+        if (fechaPedido == null) {
+            if (other.fechaPedido != null)
+                return false;
+        } else if (!fechaPedido.equals(other.fechaPedido))
+            return false;
+        if (pedido == null) {
+            if (other.pedido != null)
+                return false;
+        } else if (!pedido.equals(other.pedido))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura [id=" + id + ", valor=" + valor + ", nombreRes=" + nombreRes + ", fechaPedido=" + fechaPedido
+                + ", pedido=" + pedido + "]";
+    }
+
     
 }
--- src/main/java/CVDS/Dina/proyecto/model/Pedido.java
+++ src/main/java/CVDS/Dina/proyecto/model/Pedido.java
@@ -25,7 +25,132 @@ public class Pedido {
     @Column(name = "Pedido")
     private String nombreRestaurante;
 
+    @Column(name = "nombreComida")
+    private String nombreComida;
+
+    @Column(name = "valor")
+    private int valor;
+
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "facturaId", referencedColumnName = "PedidoId")
+    @JoinColumn(name = "id", referencedColumnName = "id")
     private Factura factura;
+
+    public Pedido(Long pedidoId, Cliente cliente, String nombreRestaurante, String nombreComida, int valor) {
+        PedidoId = pedidoId;
+        this.cliente = cliente;
+        this.nombreRestaurante = nombreRestaurante;
+        this.nombreComida = nombreComida;
+        this.valor = valor;
+    }
+
+    public Pedido() {
+    }
+
+    public Long getPedidoId() {
+        return PedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        PedidoId = pedidoId;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getNombreRestaurante() {
+        return nombreRestaurante;
+    }
+
+    public void setNombreRestaurante(String nombreRestaurante) {
+        this.nombreRestaurante = nombreRestaurante;
+    }
+
+    public String getNombreComida() {
+        return nombreComida;
+    }
+
+    public void setNombreComida(String nombreComida) {
+        this.nombreComida = nombreComida;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((PedidoId == null) ? 0 : PedidoId.hashCode());
+        result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+        result = prime * result + ((nombreRestaurante == null) ? 0 : nombreRestaurante.hashCode());
+        result = prime * result + ((nombreComida == null) ? 0 : nombreComida.hashCode());
+        result = prime * result + valor;
+        result = prime * result + ((factura == null) ? 0 : factura.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pedido other = (Pedido) obj;
+        if (PedidoId == null) {
+            if (other.PedidoId != null)
+                return false;
+        } else if (!PedidoId.equals(other.PedidoId))
+            return false;
+        if (cliente == null) {
+            if (other.cliente != null)
+                return false;
+        } else if (!cliente.equals(other.cliente))
+            return false;
+        if (nombreRestaurante == null) {
+            if (other.nombreRestaurante != null)
+                return false;
+        } else if (!nombreRestaurante.equals(other.nombreRestaurante))
+            return false;
+        if (nombreComida == null) {
+            if (other.nombreComida != null)
+                return false;
+        } else if (!nombreComida.equals(other.nombreComida))
+            return false;
+        if (valor != other.valor)
+            return false;
+        if (factura == null) {
+            if (other.factura != null)
+                return false;
+        } else if (!factura.equals(other.factura))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido [PedidoId=" + PedidoId + ", cliente=" + cliente + ", nombreRestaurante=" + nombreRestaurante
+                + ", nombreComida=" + nombreComida + ", valor=" + valor + ", factura=" + factura + "]";
+    }
+
+    
 }
