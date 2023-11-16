2a1647bc5edd400fee0f1f471998f20e68da9cda
Parralol <110953563+Parralol@users.noreply.github.com>
2023-11-16 16:29:03 -0500

    funcional

--- src/main/java/CVDS/Dina/proyecto/controller/login.java
+++ src/main/java/CVDS/Dina/proyecto/controller/login.java
@@ -1,9 +1,11 @@
 package CVDS.Dina.proyecto.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class login {
     @GetMapping("/login")
     public String login() {
